86fa06498908950438d1ccf6d1ecee1746242e6b
Sensanaty <salevic@lu-ka.me>
2022-10-02 19:46:00 +0200

    Initial commit with sensible defaults

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <ThemeToggle />
+  </header>
+</template>
+
+<script lang="ts" setup>
+  import ThemeToggle from "./ThemeToggle.vue";
+</script>
+
+<style scoped>
+  header {
+    display: flex;
+    flex-flow: row wrap;
+    background: gray;
+    padding: 0.5rem 1rem;
+    width: 100%;
+  }
+
+  button {
+    margin: 0 0 0 auto;
+  }
+</style>
---
+++ src/components/ThemeToggle.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click.prevent="toggleTheme()">Switch Theme</button>
+</template>
+
+<script lang="ts" setup>
+  const toggleTheme = () => { 
+    document.documentElement.classList.toggle("light");
+
+    if (document.documentElement.classList.contains("light")) {
+      window.localStorage.setItem("theme", "light");
+    } else { 
+      window.localStorage.setItem("theme", "dark")
+    }
+  }
+</script>
