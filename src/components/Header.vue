ab72172f9266a992f229ec6ce37637a6ecdd18bd
Sensanaty <salevic@lu-ka.me>
2022-10-04 18:40:06 +0200

    Finalize the look of the Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,23 +1,67 @@
 <template>
   <header>
+    <h1>luka salević<span aria-hidden="true">...</span><span aria-hidden="true" class="i-beam">|</span></h1>
+
+    <nav>
+      <a href="/about">about</a>
+      <span class="divider" aria-hidden="true">&middot;</span>
+      <a href="/contact">contact</a>
+      <span class="divider" aria-hidden="true">&middot;</span>
+      <a href="/work">work</a>
+    </nav>
+
     <ThemeToggle />
   </header>
 </template>
 
 <script lang="ts" setup>
+  import { ref } from "vue";
   import ThemeToggle from "./ThemeToggle.vue";
+
+  const showName = ref(true);
 </script>
 
 <style scoped>
   header {
-    display: flex;
+    display: grid;
+    grid-template: 1fr / 1fr 1fr 1fr;
     flex-flow: row wrap;
-    background: gray;
-    padding: 0.5rem 1rem;
+    align-items: center;
+    background: var(--bg-highlight);
+    padding: 0.7rem 1.5rem;
     width: 100%;
   }
 
-  button {
-    margin: 0 0 0 auto;
+  header * { font-family: "JetBrains Mono NL"; font-weight: bold; }
+
+  nav {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    align-items: center;
   }
+
+  nav a {
+    color: var(--cl);
+    text-decoration: solid underline var(--accent) 2px;
+  }
+
+  nav a:hover {
+    color: var(--accent);
+    text-decoration: solid underline var(--cl) 2px;
+  }
+
+  h1 {
+    font-size: 1.2rem;
+    margin: 0;
+    padding: 0;
+  }
+
+  span { user-select: none; cursor: default; }
+  .divider { margin: 0 6px; font-size: 1.15rem; }
+
+  button { margin: 0 0 0 auto }
+
+  .i-beam { animation: blinker 1250ms step-start infinite }
+  @keyframes blinker { 50% { opacity: 0 } }
 </style>
--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -1,15 +1,43 @@
 <template>
-  <button @click.prevent="toggleTheme()">Switch Theme</button>
+  <button 
+    @click.prevent="toggleTheme()" 
+    :aria-label="'Toggle between dark & light themes. Currently using the ' + theme + ' theme'"
+  >
+    {{theme}}
+  </button>
 </template>
 
 <script lang="ts" setup>
+  import { ref, onMounted } from 'vue';
+
+  const theme = ref();
+
+  onMounted(() => {
+    theme.value = document.documentElement.classList.contains("light") ? "light" : "dark";
+  })
+
   const toggleTheme = () => { 
     document.documentElement.classList.toggle("light");
 
     if (document.documentElement.classList.contains("light")) {
       window.localStorage.setItem("theme", "light");
+      theme.value = "light";
     } else { 
-      window.localStorage.setItem("theme", "dark")
+      window.localStorage.setItem("theme", "dark");
+      theme.value = "dark";
     }
   }
 </script>
+
+<style scoped>
+  button {
+    background: var(--bg-accent);
+    color: var(--cl);
+    text-decoration: solid underline var(--accent) 2px;
+  }
+
+  button:hover {
+    color: var(--accent);
+    text-decoration-color: var(--cl)
+  }
+</style>
