cb7699f6f97d6e013c86d438049c747945bcde7f
Luka <salevic@lu-ka.me>
2023-11-16 19:48:45 +0100

    Implement the index page & global index layout with header

---
+++ src/components/Base/ThemeToggle.vue
@@ -0,0 +1,29 @@
+<template>
+  <button
+    class="ml-auto font-black underline decoration-accent decoration-2 hover:text-accent hover:decoration-cl"
+    @click.prevent="toggleTheme"
+  >
+    {{ theme }}
+  </button>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const theme = ref<"dark" | "light">("dark");
+
+onMounted(() => theme.value = document.documentElement.classList.contains("light") ? "light" : "dark");
+
+const toggleTheme = () => {
+  const rootElement = document.documentElement;
+  rootElement.classList.toggle("light");
+
+  if (rootElement.classList.contains("light")) {
+    localStorage.setItem("theme", "light");
+    theme.value = "light";
+  } else {
+    localStorage.setItem("theme", "dark");
+    theme.value = "dark";
+  }
+}
+</script>
