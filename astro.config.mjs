3237e6dc6d16d2218f3f441b3b168db7c885bb55
Luka <salevic@lu-ka.me>
2024-11-16 14:28:34 +0100

    Add MDX to project deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwind from '@astrojs/tailwind';
 
 import svelte from '@astrojs/svelte';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), mdx()],
 });
