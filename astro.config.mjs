8e7a0c7f4bccfdf34d9d869c759c071bb07ea241
Luka <salevic@lu-ka.me>
2024-12-10 20:37:33 +0100

    Add path aliases

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
+import path from "node:path";
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
-
 import svelte from '@astrojs/svelte';
-
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte(), mdx()],
+   vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve("./src")
+      }
+    }
+   }
 });
